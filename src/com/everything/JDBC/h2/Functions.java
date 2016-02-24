15c4744180ba2b42f6b18116ce9bae4beebdc34d
makarkalancha@gmail.com <makarkalancha@gmail.com>
2016-02-24 18:23:56 -0500

    functions

--- src/com/everything/JDBC/h2/Functions.java
+++ src/com/everything/JDBC/h2/Functions.java
@@ -2,15 +2,31 @@ package com.everything.JDBC.h2;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Created by mcalancea on 2016-02-16.
  */
 public class Functions{
-    private static final StringBuilder createDateUnitTable = new StringBuilder();
-    static {
-        createDateUnitTable.append("CREATE SEQUENCE SEQ_DATEUNIT;");
-        createDateUnitTable.append("CREATE TABLE DATEUNIT(");
+    public static void main(String[] args) throws SQLException{
+        final String DB_DIR = "~/smart_finance";
+        final String DB_NAME = "finance";
+        final String DB_SCHEMA = "FINANCE";
+        final String DB_CONNECTION_IFEXISTS = "jdbc:h2:"+DB_DIR+"/"+DB_NAME+";IFEXISTS=TRUE;SCHEMA="+DB_SCHEMA;
+        final String DB_USER = "root";
+        final String DB_PASSWORD = "root";
+
+//        System.out.println("hello");
+        Connection conn = DriverManager.getConnection(DB_CONNECTION_IFEXISTS, DB_USER, DB_PASSWORD);
+        createDateUnitTable(conn);
+    }
+    
+    public static void createDateUnitTable(Connection connection) throws SQLException{
+        final StringBuilder createDateUnitTable = new StringBuilder();
+        createDateUnitTable.append("CREATE SEQUENCE IF NOT EXISTS SEQ_DATEUNIT;");
+        createDateUnitTable.append("CREATE TABLE IF NOT EXISTS DATEUNIT(");
             createDateUnitTable.append("ID BIGINT DEFAULT SEQ_DATEUNIT.NEXTVAL,");
             createDateUnitTable.append("UNITTIMESTAMP DATE NOT NULL,");
             createDateUnitTable.append("UNITYEAR INT NOT NULL,");
@@ -25,21 +41,20 @@ public class Functions{
             createDateUnitTable.append("T_UPDATEDON TIMESTAMP AS NOW(),");
             createDateUnitTable.append("PRIMARY KEY(ID,UNITTIMESTAMP),");
         createDateUnitTable.append(");");
-        createDateUnitTable.append("CREATE INDEX IDXUNITYEAR ON DATEUNIT(UNITYEAR);");
-        createDateUnitTable.append("CREATE INDEX IDXUNITMONTHOFYEAR ON DATEUNIT(UNITMONTHOFYEAR);");
-        createDateUnitTable.append("CREATE INDEX IDXUNITMONTH ON DATEUNIT(UNITMONTH);");
-        createDateUnitTable.append("CREATE INDEX IDXUNITWEEKOFYEAR ON DATEUNIT(UNITWEEKOFYEAR);");
-        createDateUnitTable.append("CREATE INDEX IDXUNITWEEK ON DATEUNIT(UNITWEEK);");
-        createDateUnitTable.append("CREATE INDEX IDXUNITDATE ON DATEUNIT(UNITDATE);");
-        createDateUnitTable.append("CREATE INDEX IDXUNITDAYOFWEEK ON DATEUNIT(UNITDAYOFWEEK);");
-        createDateUnitTable.append("CREATE INDEX IDXWEEKDAY ON DATEUNIT(WEEKDAY);");
+        createDateUnitTable.append("CREATE INDEX IF NOT EXISTS IDXUNITYEAR ON DATEUNIT(UNITYEAR);");
+        createDateUnitTable.append("CREATE INDEX IF NOT EXISTS IDXUNITMONTHOFYEAR ON DATEUNIT(UNITMONTHOFYEAR);");
+        createDateUnitTable.append("CREATE INDEX IF NOT EXISTS IDXUNITMONTH ON DATEUNIT(UNITMONTH);");
+        createDateUnitTable.append("CREATE INDEX IF NOT EXISTS IDXUNITWEEKOFYEAR ON DATEUNIT(UNITWEEKOFYEAR);");
+        createDateUnitTable.append("CREATE INDEX IF NOT EXISTS IDXUNITWEEK ON DATEUNIT(UNITWEEK);");
+        createDateUnitTable.append("CREATE INDEX IF NOT EXISTS IDXUNITDATE ON DATEUNIT(UNITDATE);");
+        createDateUnitTable.append("CREATE INDEX IF NOT EXISTS IDXUNITDAYOFWEEK ON DATEUNIT(UNITDAYOFWEEK);");
+        createDateUnitTable.append("CREATE INDEX IF NOT EXISTS IDXWEEKDAY ON DATEUNIT(WEEKDAY);");
+        try (Statement statement = connection.createStatement()){
+            statement.execute(createDateUnitTable.toString());
+        }
     }
 
-//    public static Long insertSelectDate(Date date) {
-//
-//    }
-
-    public static Long insertSelectDate(Connection conn, Date date) {
+    Long insertSelectDate(Connection conn, Date date) {
 //        return new BigInteger(String.valueOf(value)).isProbablePrime(100);
         return date.getTime();
     }
